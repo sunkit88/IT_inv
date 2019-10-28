cc7bcc7294cfcde07bcc3ec13fdc95305d7ade72
sunkit@gmail.com <Winsy767>
2019-10-28 16:44:55 +0800

    code updated

--- IT_INV.py
+++ IT_INV.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-it_inv_raw = pd.read_csv(".\IT_INV.csv")
-fin_inv_raw = pd.read_csv(".\FIN_INV.csv")
+it_inv_raw = pd.read_csv("IT_INV.csv")
+fin_inv_raw = pd.read_csv("FIN_INV.csv")
 
-it_inv = it_inv_raw[(it_inv_raw["Status (Phy.)"]=="Yes") & (it_inv_raw["Status (Fixed Asset)"] == "Yes")]
+it_inv = it_inv_raw[(it_inv_raw["Status (Phy.)"] == "Yes") & (it_inv_raw["Status (Fixed Asset)"] != "No")].copy()
 it_inv = it_inv[["Compand Code", "Asset Code", "Model", "Serial No.", "Location", "User"]]
 it_inv["Asset Code"] = it_inv["Asset Code"].apply(int)
 it_inv["Asset Code"] = "00000000" + it_inv["Asset Code"].apply(str)
@@ -12,9 +12,9 @@ it_inv["AssetCode"] = it_inv["Compand Code"].astype(str) + it_inv["Asset Code"].
 it_inv = it_inv.drop(columns=["Asset Code"])
 it_inv = it_inv.reindex(columns=["AssetCode"] + list(it_inv.columns[:-1]))
 
-fin_inv = fin_inv_raw[["Business Unit AM", "Asset ID", "Asset Description", "Custodian", "Department"]]
-fin_inv["Asset ID"] = fin_inv["Asset ID"].str[-8:]
-fin_inv["AssetCode"] = fin_inv["Business Unit AM"].astype(str) + fin_inv["Asset ID"].astype(str)
+fin_inv = fin_inv_raw[["Business Unit AM", "Asset ID", "Asset Description", "Custodian", "Department"]].copy()
+fin_inv["Asset ID"] = fin_inv.loc[:, "Asset ID"].str[-8:]
+fin_inv["AssetCode"] = fin_inv["Business Unit AM"].astype(str) + fin_inv.loc[:, "Asset ID"].astype(str)
 fin_inv = fin_inv.drop(columns=["Asset ID"])
 fin_inv = fin_inv.reindex(columns=["AssetCode"] + list(fin_inv.columns[:-1]))
 fin_inv = fin_inv[fin_inv["Business Unit AM"].isin(["DFMNM", "DFSNM", "FCGEM", "FCHIM", "FCHKM", "FCITM", "FHSTM", "FJERM", "FMACM", "FMEMM", "FPHXM", "FSAVM", "FSFOM", "FTJNM", "FVANM"])]
